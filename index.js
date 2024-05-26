d6fb38ec9ac72e507b0b56607b94846ad1712ec6
unknown <poojas1054442@gmail.com>
2024-05-26 20:46:39 +0530

    add changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputBox = document.getElementById("input-box");
     const listContainer = document.getElementById("list-container");
 
-    function addTask() {
+    window.addTask = function addTask() {
         if (inputBox.value === '') {
             alert("You must write something!");
         } else {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         inputBox.value = "";
         saveData();
-    }
+    };
 
     listContainer.addEventListener("click", function(e) {
         if (e.target.tagName === "LI") {
@@ -36,7 +36,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     showTask();
-
-    // Expose addTask to global scope
-    window.addTask = addTask;
 });
